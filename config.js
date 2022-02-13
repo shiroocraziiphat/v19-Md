1c57683c415d8967cf509c73ac1dc747adc4ebaa
shiroocraziiphat <99143773+shiroocraziiphat@users.noreply.github.com>
2022-02-14 04:27:40 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 let fs = require('fs')
 
-global.owner = ['6288279268363'] // Put your number here
+global.owner = ['6282361160044'] // Put your number here
 global.mods = [] // Want some help?
 global.prems = JSON.parse(fs.readFileSync('./src/premium.json'))
 global.APIs = { // API Prefix
@@ -36,19 +36,19 @@ leyskey = 'dappakntlll'
 bearkey = 'Apikey'
 ApiiKey = 'https://youtu.be/Sgb5BVOW66Y'
 
-namabot = 'Yuki Bot'
-namalu = 'King Of Bear'
-footer = '\n©King Of Bear'
+namabot = 'Shiro Botz'
+namalu = 'Shiro Akio'
+footer = '\n©Shiro-Md'
 kasihcaption = `Nih Sayang`
 
 // Sticker WM
 global.stiker_wait = '_*Tunggu Sebentar...*_'
-global.packname = 'YukiBot Multi device'
-global.author = 'King Of Bear'
+global.packname = 'ShiroBotz Md'
+global.author = 'Loppyuuu<3'
 global.email = 'drakipul1016@gmail.com'
 global.fla = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
-global.wm = '©King Of Bear Bot-Md'
-global.media = 'https://telegra.ph/file/5d45a3d1e12726d98c5e4.jpg'
+global.wm = '©Shiro-Md'
+global.media = 'https://images.app.goo.gl/oJmEcfg3YpbpXX3r9'
 
 global.wait = '_*Tunggu Sebentar Sedang Di Proses...*_'
 global.eror = '_*Server Error*_'
